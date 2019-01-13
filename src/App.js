8d57a508d59e2cf0f2456432672ede2e36d5980c
Carlos Garcia <gtxcarl9@gmail.com>
2019-01-13 09:36:07 +0100

    react-router-dom changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import Home from './Views/Home/Index';
+import GitHub from "./Views/GitHub/Index";
+import Contacto from "./Views/Contact/Index";
+import About from "./Views/About/Index";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 class App extends Component {
 
@@ -7,7 +11,14 @@ class App extends Component {
 
         return (
             <div className="bg-white">
-                <Home/>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path={'/'} exact={true} component={Home}/>
+                        <Route path={'/github'} exact={true} component={GitHub}/>
+                        <Route path={'/contacto'} exact={true} component={Contacto}/>
+                        <Route path={'/about'} exact={true} component={About}/>
+                    </Switch>
+                </BrowserRouter>
              </div>
         )
     }
